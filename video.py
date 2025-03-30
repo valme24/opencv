f90e1a3a7c956215f6d840cc50c65a85bff5f746
valme24 <vansh.goel.0403@gmail.com>
2025-03-30 19:34:50 +0530

    video diplayed successfully

---
+++ video.py
@@ -0,0 +1,14 @@
+import os
+import cv2 as cv
+vid = cv.VideoCapture("videos/dog.mp4")
+ret =True
+while True :
+    ret,frame = vid.read()
+    if not ret :
+        break
+    cv.imshow("video",frame)
+    cv.waitKey(1)
+
+
+
+
