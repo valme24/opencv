581bc97708ff319738f65746fcd1baf13e6c0be3
valme24 <vansh.goel.0403@gmail.com>
2025-03-30 11:50:32 +0530

    read images

--- read.py
+++ read.py
@@ -1,4 +1,16 @@
 import cv2 as cv
-img = cv.imread("images/cat.jpeg")
-cv.imshow("Cat",img)
-cv.waitKey(0)
+# img = cv.imread("images/cat.jpeg")
+
+capture = cv.VideoCapture("vidoes/dog.mp4")
+print("video is read")
+while True:
+    isTrue,frame = capture.read()
+    print("videos are frames are currently showing")
+    cv.imshow("Vidoes",frame)
+
+    if cv.waitKey(20) &  0xFF ==ord('d'):
+        break 
+capture.release()
+cv.destroyAllWindows()
+# cv.imshow("Cat",img)------------------------
+# cv.waitKey(0)
